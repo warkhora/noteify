fed9071f9b155ad6414ec90be6c5a665dbecff48
warkhora <joseffretland@gmail.com>
2026-05-21 22:01:37 +0200

    Update Firebase configuration with new credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "DIN_API_KEY",
-  authDomain: "DITT_DOMENE",
-  projectId: "DITT_PROSJEKT_ID",
-  storageBucket: "DITT_BUCKET",
-  messagingSenderId: "DIN_SENDER_ID",
-  appId: "DIN_APP_ID"
+  apiKey: "AIzaSyD0RxQ4CpNa98wR-oW7xETNoZbD2qxEWsY",
+  authDomain: "noteify-e9a57.firebaseapp.com",
+  projectId: "noteify-e9a57",
+  storageBucket: "noteify-e9a57.firebasestorage.app",
+  messagingSenderId: "164619394184",
+  appId: "1:164619394184:web:b47eba6dde6d1b41cc933d",
+  measurementId: "G-6HHPTBEJQQ"
 };
 
 export const app = initializeApp(firebaseConfig);
